f389a2c901f2c979c542f9cd6d598a2598ad0773
Evgeniya <evavong363@gmail.com>
2022-11-20 16:04:38 +0300

    Added Task 8

---
+++ Task008/Program.cs
@@ -0,0 +1,20 @@
+﻿// 8. Написать программу, которя на вход принимает число N,
+// а на выходе показывает все четные числа от 1 до N
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Способ 1 (цикл while c шагом 2): ");
+int count=2;
+while(count<=N)
+{
+  Console.Write($"{count}  ");
+  count+=2;
+}
+
+Console.WriteLine();
+Console.WriteLine("Способ 2 (цикл for с проверкой остатка от деления на 2): ");
+for(count=1;count<=N;count++)
+{
+  if(count%2==0) Console.Write($"{count}  ");
+}
