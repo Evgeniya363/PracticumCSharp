cb1f9e4c8937141e5978110dca92c15a8bc72d9c
Evgeniya <evavong363@gmail.com>
2022-11-21 21:53:33 +0300

    Added Task009

---
+++ Task009/Program.cs
@@ -0,0 +1,9 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 100 || num > 999) 
+  Console.Write("Число не является трехзначным");
+else
+  Console.Write($"Последняя цифра числа: {num%10}");
+
