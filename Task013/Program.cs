4ccaa8e858f110207ec4044da558809043854639
Evgeniya <evavong363@gmail.com>
2022-11-26 12:52:24 +0300

    Added Task13

---
+++ Task013/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, принимающую на входе  
+// число и показывающую на выходе 
+// 3-ю цифру или сообщение, что такой цифры нет
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int numAbs = Math.Abs(num);
+while(numAbs>=1000)
+{
+  numAbs=numAbs/10;
+}
+Console.WriteLine(numAbs);
+
+if (numAbs <100)
+  Console.WriteLine("Неверный ввод");
+else
+  Console.WriteLine($"Третья цифра: {numAbs % 10}");
