dacfc36b913bef52458dff8f8c1992dfcaef39b1
Evgeniya <evavong363@gmail.com>
2022-11-26 12:41:43 +0300

    Modified Task10

---
+++ Task010/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу, принимающую на входе  
+// трехзначное число и показывающую на выходе 
+// 2-ю цифру
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int numAbs = Math.Abs(num);
+if (numAbs<100 || numAbs>1000)
+  Console.WriteLine("Неверный ввод");  
+else
+  Console.WriteLine($"Вторая цифра: {Math.Abs((num%100 - num%10)/10)}");
