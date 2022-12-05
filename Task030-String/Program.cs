a7d3abd6d8a671af55620449f806a1076b16b195
Evgeniya <evavong363@gmail.com>
2022-12-06 00:16:38 +0300

    Modified Tasks030-031

---
+++ Task030-String/Program.cs
@@ -0,0 +1,13 @@
+﻿string str = "Светок, Боб, Жека, Игорёха, Танюхен.";
+string str2 = "1, 2, 5, 7";
+string[] nums = str2.Split(new char[]{' ',',','.'}, System.StringSplitOptions.RemoveEmptyEntries);
+foreach (var num in nums)
+{
+  Console.WriteLine($"Значение: {num}");
+}
+int[] arr = nums.Select(Int32.Parse).ToArray();
+foreach (int var in arr)
+{
+  Console.Write($"{var}  ");
+}
+int[] num1 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
